c759caf9c380430544ca62a1c72f1ea2bb2cb267
LaniJeong <123913969+LaniJeong@users.noreply.github.com>
2023-05-24 17:18:37 +0900

    5월 24일_1차 저장

---
+++ Day06/Day06_Web/aspnet02_boardapp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using aspnet02_boardapp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnet02_boardapp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext       // 1. ASP.NET Identity: DBContext -> Identity DbContext 쓰는것과 동일
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Board> Boards { get; set; }
+    }
+}
---
+++ Day06/Day06_Web/aspnet02_boardapp/Models/Board.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace aspnet02_boardapp.Models
+{
+    // 게시판을 위한 테이블 매핑 정보
+    public class Board
+    {
+        [Key] // PK
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "아이디를 입력하세요")] // Not Null
+        [DisplayName("아이디")]
+        public string UserId { get; set; }
+        [DisplayName("이름")]
+        public string? UserName { get; set; } // Null 허용
+
+        [Required(ErrorMessage = "제목을 입력하세요")] // Not Null
+        [MaxLength(200)] // == Varchar(200)
+        [DisplayName("제목")]
+        public string Title { get; set; }
+        [DisplayName("조회")]
+        public int ReadCount { get; set; }
+        [DisplayName("작성일")]
+        public DateTime PostDate { get; set; }
+        [DisplayName("게시글")]
+        public string Contents { get; set; }
+    }
+}
